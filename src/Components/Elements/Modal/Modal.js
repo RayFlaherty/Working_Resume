c3d5f16c0f33af23bef91c509cb566403b341ad4
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-04-05 11:11:07 -0500

    nav bar and modal started, some overall styling

---
+++ src/Components/Elements/Modal/Modal.js
@@ -0,0 +1,14 @@
+import Card from "../../UI/Card";
+import './Modal.css'
+
+const Modal = () => {
+  return (
+    <Card>
+      <div className="modal-body">
+                Text
+      </div>
+    </Card>
+  );
+};
+
+export default Modal;
---
+++ src/Components/Elements/Nav.js
@@ -0,0 +1,9 @@
+import Card from "../UI/Card";
+
+const Nav = () => {
+    return <Card>
+        <h1>Raymond Flaherty</h1>
+    </Card>
+}
+
+export default Nav;
